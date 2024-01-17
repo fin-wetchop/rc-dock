eea1c44caa6fb729ffab9e0298ad9e891e0c7e2d
Fin <vladislav.2002.vn@gmail.com>
2024-01-17 23:49:18 +0200

    test.

--- src/DockDropEdge.tsx
+++ src/DockDropEdge.tsx
@@ -118,16 +118,18 @@ export class DockDropEdge extends React.PureComponent<DockDropEdgeProps, any> {
   }
 
   onDragOver = (e: DragState) => {
+    console.log(e, "drag over")
+
     let {panelData, panelElement, dropFromPanel} = this.props;
     let dockId = this.context.getDockId();
     let draggingPanel = DragState.getData('panel', dockId);
 
     let fromGroup = this.context.getGroup(dropFromPanel.group);
     let toGroup = this.context.getGroup(panelData.group);
-    // if (draggingPanel && draggingPanel.parent?.mode === 'float') {
-    //   // ignore float panel in edge mode
-    //   return;
-    // }
+    if (draggingPanel && draggingPanel.parent?.mode === 'float') {
+      // ignore float panel in edge mode
+      return;
+    }
     let {
       direction,
       mode,
